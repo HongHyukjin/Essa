01460a81a2c88e5e64f675caeb7e2bb723506948
Younghyeon22 <marinoma@naver.com>
2023-06-17 23:17:43 +0900

    2023-06-17

--- essa/src/components/wrap/MainComponent.jsx
+++ essa/src/components/wrap/MainComponent.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Section5Component from './main/Section5Component';
 
 export default function MainComponent () {
     return (
         <main id='main'>
-    
+            <Section5Component/>
         </main>
     );
 };
