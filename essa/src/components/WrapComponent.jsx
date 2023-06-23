d6d1130fecf1eadd4845c6f376762f62f7c7eae1
DDULKI <seulki4994@naver.com>
2023-06-23 17:28:32 +0900

    0623 비디오 높이가 안잡힘 살려줘

--- essa/src/components/WrapComponent.jsx
+++ essa/src/components/WrapComponent.jsx
@@ -4,6 +4,7 @@ import MainComponent from './wrap/MainComponent';
 import FooterComponent from './wrap/FooterComponent';
 import Section4Component from './wrap/main/Section4Component';
 import Section6Component from './wrap/main/Section6Component';
+import Section11Component from './wrap/main/Section11Component';
 
 export default function WrapComponent ()  {
     return (
@@ -12,6 +13,7 @@ export default function WrapComponent ()  {
             <MainComponent/>
             <Section4Component/>
             <Section6Component/>
+            <Section11Component/>
             <FooterComponent/>
         </div>
     );
